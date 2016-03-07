2ea2dddc4d6fd71ebefdb001aca59d91e8268020
Sergio <Sergio@Sergio-PC>
2016-03-07 19:12:45 +0100

    Primer commit

---
+++ ejercicio10/src/ejercicio10/principal.java
@@ -0,0 +1,12 @@
+package ejercicio10;
+
+public class principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		System.out.println("Soy sergio");
+	}
+
+}
